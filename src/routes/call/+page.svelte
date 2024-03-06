696fbbc018d8e9bce745bef4d7451ef035ff1ba0
Ewa Wrobel <ewa.wrobel@tink.com>
2024-03-06 10:48:34 +0100

    single page styling

--- src/routes/call/+page.svelte
+++ src/routes/call/+page.svelte
@@ -2,11 +2,30 @@
 	export let data;
 </script>
 
-<div>
-	{#each data.summaries as { content, title }}
-		<div>
-      <h2>{title}</h2>
-      {@html content}
-    </div>
-	{/each}
+
+  <div class="layout">
+    <main>
+        {#each data.summaries as { content, title }}
+          <div class="layout-element">
+            <h2>{title}</h2>
+            {@html content}
+          </div>
+        {/each}
+    </main>
   </div>
+  
+  <style>
+    @media (min-width: 640px) {
+      .layout {
+        display: grid;
+        gap: 2em;
+        grid-template-columns: 1fr 16em;
+      }
+      .layout-element {
+        border: 3px solid hsl(0, 0%, 40%);
+        border-radius: 4px;
+        padding: 20px;
+        margin: 10px 0;
+      }
+    }
+  </style>
