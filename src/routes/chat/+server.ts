b08739ba36775f230898426e182d6d5193ff80db
Ewa Wrobel <ewa.wrobel@tink.com>
2024-03-07 12:00:30 +0100

    test for opanAI

--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -1,10 +1,18 @@
 import { json } from '@sveltejs/kit';
 import * as client from '$lib/server/chatClient';
+import { openai }  from '$lib/server/openAi';
+const aiData = async () => { const response = await openai.chat.completions.create({
+	messages: [{ role: "user", content: "Say this is a test" }],
+	model: "gpt-3.5-turbo",
+	});
+	console.log(response); 
+}; 
 
 export function GET() {
+	aiData()
 	const number = Math.floor(Math.random() * 6) + 1;
-
 	return json(number);
+
 }
 
 export async function POST({ request }) {
