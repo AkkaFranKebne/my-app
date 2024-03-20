bd077d3e7656de1a0e729f7e8431e94a3f308ca8
Ewa Wrobel <ewa.wrobel@tink.com>
2024-03-20 12:30:34 +0100

    on click animation

--- src/routes/AddressBox.svelte
+++ src/routes/AddressBox.svelte
@@ -1,18 +1,27 @@
 <script>
-  import { createEventDispatcher } from 'svelte'
-  const dispatch = createEventDispatcher()
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+  let clicked = false;
+  let hovered = false;
 
   function mouseenter() {
-     dispatch('mouseenter')
+     dispatch('mouseenter');
+     hovered = true;
   }
   function focus() {
-     dispatch('focus')
+     dispatch('focus');
+     hovered = true;
   }
   function mouseout() {
-     dispatch('mouseout')
+     dispatch('mouseout');
+     hovered = false;
   }
   function blur() {
-     dispatch('blur')
+     dispatch('blur');
+     hovered = false;
+  }
+  function click() {
+     clicked = true;
   }
 </script>
 
@@ -22,13 +31,36 @@
   on:focus={focus} 
   on:mouseout={mouseout} 
   on:blur={blur} 
+  on:click={click}
   class="{$$restProps.class || ''}" 
+  class:clicked
+  class:hovered
   href="{$$restProps.href || ''}">
   {$$restProps.text || ''}
 </a>
 
 
 <style>
+
+    @keyframes wobble {
+      0% { transform: translateX(0%); }
+      15% { transform: translateX(-25%) rotate(-5deg); }
+      30% { transform: translateX(20%) rotate(3deg); }
+      45% { transform: translateX(-15%) rotate(-3deg); }
+      60% { transform: translateX(10%) rotate(2deg); }
+      75% { transform: translateX(-5%) rotate(-1deg); }
+      100% { transform: translateX(0%); }
+    }
+
+    @keyframes bounce {
+      70% { transform:translateY(0%); }
+      80% { transform:translateY(-15%); }
+      90% { transform:translateY(0%); }
+      95% { transform:translateY(-7%); }
+      97% { transform:translateY(0%); }
+      99% { transform:translateY(-3%); }
+      100% { transform:translateY(0); }
+    }
     .element {
         background-color: hsl(0, 0%, 100%);
         color: hsl(0, 0%, 53%);
@@ -44,10 +76,18 @@
         &:hover {
           cursor: pointer;
         }
+
+        &.clicked {
+          animation: wobble 2s ease infinite;
+        }
+
+        &.hovered {
+          animation:  bounce 0.5s ease;
+        }
   
         @media (min-width: 640px) {
           margin: 0 10px;
-      }
+        }
       }
   </style>
   
