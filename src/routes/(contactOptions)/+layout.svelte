65425e914783ce48687c500cb052d37ba246d041
Ewa Wrobel <ewa.wrobel@tink.com>
2024-03-26 16:49:51 +0100

    key block transitions

--- src/routes/(contactOptions)/+layout.svelte
+++ src/routes/(contactOptions)/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
 	import '../../app.pcss';
+	import { onMount } from 'svelte';
+	import { typewriter } from './typewriter';
+
+	const messages = [
+	"hello...",
+	"how are you?",
+	"welcome...",
+	];
+
+	let i = -1;
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			i += 1;
+			i %= messages.length;
+		}, 2500);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
 <nav>
@@ -7,6 +28,11 @@
 	<a href="/chat">chat</a>
 	<a href="/message">message</a>
 	<a href="/call">call</a>
+	{#key i}
+		<p in:typewriter={{ speed: 10 }}>
+			{messages[i] || ''}
+		</p>
+	{/key}
 </nav>
 <!-- to show pages' content wherever but not main page -->
 <slot />
